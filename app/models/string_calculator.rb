70cd5eb31f6b61c60f63492e0ce078fc823e981e
vishal <vishal24tanwar@gmail.com>
2025-01-01 10:11:43 +0530

    bussiness logic for adding number

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,2 +1,25 @@
 class StringCalculator < ApplicationRecord
+	def self.add(numbers)
+	  return 0 if numbers.empty?
+
+	  num_list = numbers.scan(/-?\d+/)
+
+	  total = 0
+	  negatives = []
+	  num_list.each do |num_str|
+	    next if num_str.empty?  
+	    num = num_str.to_i
+	    if num < 0
+	      negatives << num
+	    else
+	      total += num
+	    end
+	  end
+
+	  if negatives.any?
+	    raise "negative numbers not allowed #{negatives.join(',')}"
+	  end
+
+	  total
+	end
 end
